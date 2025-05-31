b95a27c462ea887579165587cb417a4e4e707dc4
basanta44 <90908863+basanta44@users.noreply.github.com>
2025-05-31 13:07:56 +0545

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const formatSeconds = (seconds) => {
 async function getSongs(folder) {
   currFolder = folder;
   let a = await fetch(
-    `Spotify-Clone/${folder}/`
+    `spotify-clone/${folder}/`
   );
   let response = await a.text();
   let div = document.createElement('div');
@@ -100,7 +100,7 @@ const playMusic = (track, pause = false) => {
 
 async function displayAlbums() {
   let a = await fetch(
-    'Spotify-Clone/songs/'
+    'spotify-clone/songs/'
   );
   let response = await a.text();
   let div = document.createElement('div');
@@ -113,7 +113,7 @@ async function displayAlbums() {
     if (e.href.includes('/songs/')) {
       let folder = e.href.split('/').slice(-1)[0];
       let a = await fetch(
-        `Spotify-Clone/songs/${folder}/info.json`
+        `spotify-clone/songs/${folder}/info.json`
       );
       let response = await a.json();
       let cardContainer = document.querySelector('.cardContainer');
@@ -132,7 +132,7 @@ async function displayAlbums() {
 }
 
 async function main() {
-  await getSongs('Spotify-Clone/songs/Recovery');
+  await getSongs('spotify-clone/songs/Recovery');
 
   await displayAlbums();
 

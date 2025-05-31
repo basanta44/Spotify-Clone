8eee4c5e5711d65aaeeb74e890190a5baac1e476
basanta44 <90908863+basanta44@users.noreply.github.com>
2025-05-31 13:39:10 +0545

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const formatSeconds = (seconds) => {
 async function getSongs(folder) {
   currFolder = folder;
   let a = await fetch(
-    `https://vercel.com/basanta44s-projects/spotify-clone/${folder}/`
+    `/api/${folder}/`
   );
   let response = await a.text();
   let div = document.createElement('div');
